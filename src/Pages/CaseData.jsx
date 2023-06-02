41d94c099cd8cb828d22fda2d8efb50bc5cbd599
theruk17 <therukza@hotmail.com>
2023-06-02 15:04:25 +0700

    update020623

--- src/Pages/CaseData.jsx
+++ src/Pages/CaseData.jsx
@@ -6,7 +6,7 @@ import { Space, Table, Switch, Modal, Divider, message, Row, Col, Form, Checkbox
 
 import '../App.css';
 
-const API_URL = 'd1hcfk5kl1j97j.cloudfront.net';
+const API_URL = 'https://d1hcfk5kl1j97j.cloudfront.net';
 
 const Brand = [
   { val: 'AEROCOOL' },
